3f395d2cb2091bc088be251a91e4ca5b8abdb646
princess <hatesnna@gmail.com>
2026-09-22 16:06:52 +0700

    Pertemuan 2 latihan PHP dasar

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/latihan-php', function () {
+    $nama = 'Devina Rindumasha';
+    $nilai = [50, 55, 60, 65, 70];
+ 
+    $hitungRataRata = function (array $data): float {
+        $total = 0;
+        foreach ($data as $angka) {
+            $total += $angka;
+        }
+        return $total / count($data);
+    };
+ 
+    $rataRata = $hitungRataRata($nilai);
+    if ($rataRata >= 75) {
+        $status = 'Lulus';
+    } else {
+        $status = 'Perlu Perbaikan';
+    }
+ 
+    return view('latihan-php', compact(
+        'nama', 'nilai', 'rataRata', 'status'
+    ));
 });
